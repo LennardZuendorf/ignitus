f91e95918577e0fc0d839cf0032089e1762c9c7d
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-04-18 15:07:17 +0200

    updated config

--- app.config.ts
+++ app.config.ts
@@ -1,41 +1,47 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Ignitus',
+    description: 'Personal project showcase',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
     },
+    logo: '/logo.svg',
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'ignitr logo' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: "find me at"// string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      mail:{
+        icon: 'uil:envelope',
+        label: 'Mail',
+        href: 'mailto:lennard@zuendorf.me'
+      },
+      github: {
+        icon: 'uil:github',
+        label: 'GitHub',
+        href: 'https://www.github.com/lennardzuendorf'
+        },
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://www.linkedin.com/in/lennardzuendorf'
       }
-    },
-    form: {
-      successMessage: 'Message sent. Thank you!'
     }
   }
 })
