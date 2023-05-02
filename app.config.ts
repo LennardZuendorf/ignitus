4ced31abac24fa2ef30ae4d3d69830093378d5ec
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-05-02 19:14:05 +0200

    fixes

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 export default defineAppConfig({
   alpine: {
     title: 'Ignitus',
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 export default defineNuxtConfig({
   extends: ['@nuxt-themes/alpine', '@nuxt-themes/elements']
 })
