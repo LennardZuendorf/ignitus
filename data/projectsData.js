be4b0dbafbeafecf0615a9f7e96405731671639a
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-05-14 17:55:03 +0200

    adding content

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,20 +2,20 @@ const projectsData = [
   {
     title: 'habitus project',
     description: `Showcase for a Java Spring backend, born out of a University Project. Personal training ground.`,
-    imgSrc: '/static/images/projects/habitus.png',
+    imgSrc: '/static/images/habitus/intro.png',
     href: '/blog/habitus',
   },
   {
     title: 'quaestio',
     description: `A beautifully designed game about deep questions. Build on Flutter with FlutterFlow. Still in Development.`,
-    imgSrc: '/static/images/projects/quaestio.jpg',
+    imgSrc: '/static/images/quaestio/intro.jpg',
     href: '/blog/quaestio',
   },
   {
     title: 'twinkles',
     description: `Idea for a modern dating app, with real location based dating. Build on Flutter with FlutterFlow.
     Project currently paused.`,
-    imgSrc: '/static/images/projects/twinkles.jpg',
+    imgSrc: '/static/images/twinkles/intro.jpg',
     href: '/blog/twinkles',
   },
 ]
