d7dcdb70d09ed39bef7319209717fd2a37510b8d
Lennard Zündorf <l.p.zuendorf@gmail.com>
2023-05-14 22:42:19 +0200

    Update Tag.js

--- components/Tag.js
+++ components/Tag.js
@@ -3,7 +3,6 @@ import { kebabCase } from 'pliny/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link
-      href={`/tags/${kebabCase(text)}`}
       className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-accent-color dark:hover:text-accent-color-dark"
     >
       {text.split(' ').join('-')}
