89a626fc8b22f5a3e3a2be6a156f3e354e336eb7
LennardZuendorf <l.p.zuendorf@gmail.com>
2023-05-19 11:42:41 +0200

    adding new pages/projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'nubes',
+    description: `Idea for a modern dating app, with real location based dating. Built on Flutter with FlutterFlow.`,
+    imgSrc: '/static/images/twinkles/intro.jpg',
+    href: '/blog/nubes',
+  },
+  {
     title: 'habitus project',
     description: `Showcase for a Java Spring backend, born out of a University Project. Personal training ground.`,
     imgSrc: '/static/images/habitus/intro.png',
@@ -10,6 +16,12 @@ const projectsData = [
     description: `A beautifully designed game about deep questions. Built on Flutter with FlutterFlow. Still in Development.`,
     imgSrc: '/static/images/quaestio/intro.jpg',
     href: '/blog/quaestio',
+  },
+  {
+    title: 'ignitus',
+    description: `Idea for a modern dating app, with real location based dating. Built on Flutter with FlutterFlow.`,
+    imgSrc: '/static/images/twinkles/intro.jpg',
+    href: '/blog/ignitus',
   },
   {
     title: 'twinkles',
